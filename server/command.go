6020a2f4c7eca6538a35c0b42cbbc4bc4d06696c
Mario de Frutos <mario@defrutos.org>
2019-11-28 11:15:45 +0100

    Multiple changes in command
    - Added list commands to show the subscriptions
    - Fixed problem when an error occur (return after the postCommand)

--- server/command.go
+++ server/command.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/mattermost/mattermost-server/model"
@@ -47,28 +48,59 @@ func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*mo
 	case "subscribe":
 		//https://play.grafana.org/render/d-solo/000000012/grafana-play-home?orgId=1&from=&to=1574720321201&panelId=2&width=1000&height=500&tz=Europe%2FMadrid
 		if len(parameters) != 3 {
-			p.postCommandResponse(args, "You have to pass the panel URL, channel and refresh period parameters")
+			p.postCommandResponse(args, "You have to pass the panel channel, URL and refresh interval parameters")
+			return &model.CommandResponse{}, nil
 		}
-		err := p.Subscribe(parameters[0], parameters[1], parameters[2])
+		err := p.Subscribe(args.TeamId, parameters[0], parameters[1], parameters[2])
 		if err != nil {
 			p.postCommandResponse(args, err.Error())
+			return &model.CommandResponse{}, nil
 		}
 		err = p.RefreshSubscriptions()
 		if err != nil {
 			p.postCommandResponse(args, err.Error())
+			return &model.CommandResponse{}, nil
 		}
+		p.postCommandResponse(args, "Subscription created succesfully")
+	case "list":
+		subsText, err := p.showSubscriptions()
+		if err != nil {
+			p.postCommandResponse(args, err.Error())
+			return &model.CommandResponse{}, nil
+		}
+		p.postCommandResponse(args, subsText)
 	case "unsubscribe":
 		if len(parameters) != 1 {
 			p.postCommandResponse(args, "You have to pass the channel parameter")
+			return &model.CommandResponse{}, nil
 		}
-		err := p.Unsubscribe(parameters[0])
+		err := p.Unsubscribe(args.TeamId, parameters[0])
 		if err != nil {
 			p.postCommandResponse(args, err.Error())
+			return &model.CommandResponse{}, nil
 		}
+		p.postCommandResponse(args, "Subscription removed succesfully")
 	case "refresh":
 		p.RefreshSubscriptions()
 	}
 
 	return &model.CommandResponse{}, nil
 
+}
+
+func (p *Plugin) showSubscriptions() (string, error) {
+	subscriptions, err := p.GetSubscriptions()
+	if err != nil {
+		return "", err
+	}
+	txt := ""
+	if len(subscriptions) == 0 {
+		txt = "Currently there are no subscriptions"
+	} else {
+		txt = "### Subscriptions\n"
+	}
+	for _, sub := range subscriptions {
+		txt += fmt.Sprintf("* `%s` - %s\n", strings.Trim(sub.ChannelName, "/"), sub.PanelURL)
+	}
+	return txt, nil
 }

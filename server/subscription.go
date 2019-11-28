f99e40ecb9864884e83b88ef664c19b7abbde119
Mario de Frutos <mario@defrutos.org>
2019-11-28 11:13:44 +0100

    Multiple changes in subscriptions
    - Merge storage and subscription files
    - Now we pass the TeamID and ChannelName to retrieve the selected
    channel

--- server/subscription.go
+++ server/subscription.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"strconv"
+
+	"github.com/mattermost/mattermost-server/model"
 )
 
 const (
@@ -14,19 +17,25 @@ const (
 // Subscription blabla
 type Subscription struct {
 	ChannelID         string
+	ChannelName       string
 	PanelURL          string
 	RefreshPeriod     int
 	LastTimeRefreshed int
 }
 
 // Subscribe blabla
-func (p *Plugin) Subscribe(panelURL string, channel string, refreshPeriod string) error {
+func (p *Plugin) Subscribe(teamID string, channelName string, panelURL string, refreshPeriod string) error {
 	refreshPeriodNumeric, err := strconv.Atoi(refreshPeriod)
 	if err != nil {
 		return err
 	}
+	channel, appError := p.API.GetChannelByName(teamID, channelName, false)
+	if appError != nil {
+		return errors.New(appError.Error())
+	}
 	newSubscription := &Subscription{
-		ChannelID:         channel,
+		ChannelID:         channel.Id,
+		ChannelName:       channel.Name,
 		PanelURL:          panelURL,
 		RefreshPeriod:     refreshPeriodNumeric,
 		LastTimeRefreshed: -1,
@@ -41,7 +50,7 @@ func (p *Plugin) Subscribe(panelURL string, channel string, refreshPeriod string
 	}
 	exists := false
 	for index, s := range subscriptions {
-		if s.ChannelID == channel {
+		if s.ChannelID == channel.Id {
 			subscriptions[index] = newSubscription
 			exists = true
 			break
@@ -103,14 +112,18 @@ func (p *Plugin) GetSubscriptions() ([]*Subscription, error) {
 }
 
 // Unsubscribe blabla
-func (p *Plugin) Unsubscribe(channelID string) error {
+func (p *Plugin) Unsubscribe(teamID string, channelName string) error {
 	subscriptions, err := p.GetSubscriptions()
 	if err != nil {
 		return err
 	}
+	channel, appError := p.API.GetChannelByName(teamID, channelName, false)
+	if appError != nil {
+		return errors.New(appError.Error())
+	}
 
 	for index, s := range subscriptions {
-		if s.ChannelID == channelID {
+		if s.ChannelID == channel.Id {
 			subscriptions = append(subscriptions[:index], subscriptions[index+1:]...)
 		}
 	}
@@ -119,6 +132,54 @@ func (p *Plugin) Unsubscribe(channelID string) error {
 	if err != nil {
 		return err
 	}
+
+	payload := make(map[string]interface{})
+	payload["channelTarget"] = channel.Id
+
+	p.API.PublishWebSocketEvent("remove_grafana_subscription",
+		payload,
+		&model.WebsocketBroadcast{ChannelId: channel.Id})
+
+	return nil
+}
+
+// RefreshSubscriptions blabla
+func (p *Plugin) RefreshSubscriptions() error {
+	// TODO Get current time and refresh
+	subscriptions, err := p.GetSubscriptions()
+	if err != nil {
+		return err
+	}
+
+	if len(subscriptions) > 0 {
+		p.RefreshSubscription(subscriptions[0])
+	}
+
+	return nil
+}
+
+// RefreshSubscription blabla
+func (p *Plugin) RefreshSubscription(subscription *Subscription) error {
+	// TODO Retrieve the Grafana image and send it again using the websocket
+	panelImage, err := p.loadImageFromURL(subscription.PanelURL)
+	if err != nil {
+		return err
+	}
+
+	panelImageBase64, err := p.encodeImageToBase64(panelImage)
+	if err != nil {
+		return err
+	}
+
+	payload := make(map[string]interface{})
+	payload["image"] = panelImageBase64
+	payload["channelTarget"] = subscription.ChannelID
+
+	p.API.PublishWebSocketEvent("update_grafana_subscription",
+		payload,
+		&model.WebsocketBroadcast{ChannelId: subscription.ChannelID})
+
+	p.API.LogInfo("Image sent through the websocket...")
 
 	return nil
 }
